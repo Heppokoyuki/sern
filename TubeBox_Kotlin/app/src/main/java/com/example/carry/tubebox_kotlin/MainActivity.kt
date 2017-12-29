f58d7683d9afe78719688d290733ea0641e5a97c
DESKTOP-GIGDTT5\carry <car.ryu.ryu.ryu@outlook.com>
2017-12-29 17:11:33 +0900

    [fix]わかりにくい関数名の変更

--- TubeBox_Kotlin/app/src/main/java/com/example/carry/tubebox_kotlin/MainActivity.kt
+++ TubeBox_Kotlin/app/src/main/java/com/example/carry/tubebox_kotlin/MainActivity.kt
@@ -9,10 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener { sendAndConnect()}
+        button.setOnClickListener {Connect()}
         button2.setOnClickListener {callKuSocket.sendMessage(editText3.text.toString())}
     }
-    fun sendAndConnect(){
+    fun Connect(){
         val ip = editText.text.toString()
         val po = editText2.text.toString()
         callKuSocket.connectWebSocket(ip, po)
